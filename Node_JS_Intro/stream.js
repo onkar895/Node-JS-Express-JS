50c610bb6a5b209f06e75ee5bc71122d7ab43c52
Onkar Karale <onkarkarale4@gmail.com>
2023-04-30 02:52:10 +0530

    Intro to Node JS

--- Node_JS_Intro/stream.js
+++ Node_JS_Intro/stream.js
@@ -1,10 +1,14 @@
 const fs = require('fs')
+// (file system) module which allows you to read and write files.
 
 const readStream = fs.createReadStream('./Docs/blogTwo.txt', { encoding: 'utf-8' })
+// The { encoding: 'utf-8' } option specifies that the file should be read as a UTF-8 encoded text file.
+
 // readStream.on('data', (chunkOfData) => {
 //   console.log(' \n New chunk of data received \n')
 //   console.log(chunkOfData)
-// //   console.log(chunkOfData.toString())
+//   console.log(chunkOfData.toString())   
+//   To avoid using Tostring everytime we have used 'encoding: utf - 8' above.
 // })
 
 // writeStream.write(' \n New chunk of data written \n')
@@ -12,3 +16,6 @@ const readStream = fs.createReadStream('./Docs/blogTwo.txt', { encoding: 'utf-8'
 
 const writeStream = fs.createWriteStream('./Docs/blogThree.txt')
 readStream.pipe(writeStream)
+// readStream.pipe(writeStream) "pipes" the data from the read stream to the write stream, effectively copying the contents of "blogTwo.txt" to "blogThree.txt". 
+// This is a more efficient way to copy the file contents than reading the entire file into memory and then writing it to the new file.
+// By using pipe method we can directly replace the data from one file to another.
